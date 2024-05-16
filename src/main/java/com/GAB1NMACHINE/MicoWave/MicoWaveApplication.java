ae905614cf0fb3ccb2fcd68aa93390fc67d1bc87
Paul Preuvost <118807111+PaulPreuvost@users.noreply.github.com>
2024-05-16 11:32:07 +0200

    create project

---
+++ src/main/java/com/GAB1NMACHINE/MicoWave/MicoWaveApplication.java
@@ -0,0 +1,13 @@
+package com.GAB1NMACHINE.MicoWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicoWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicoWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GAB1NMACHINE/MicoWave/MicoWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GAB1NMACHINE.MicoWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicoWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
