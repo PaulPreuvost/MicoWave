dcaa7b2c38656525931a73eba3e4c59142d1ff86
moutoussamy-oriane <118806763+OrianeMoutoussamy@users.noreply.github.com>
2024-05-17 09:44:55 +0200

    MAJ : - authenticate -> register - css register - lien login/register

--- src/main/java/com/gab1nmachine/register/RegisterController.java
+++ src/main/java/com/gab1nmachine/register/RegisterController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RegisterController{
-    @GetMapping("/authenticate")
+    @GetMapping("/register")
     public String authenticate() {
-        return "authenticate";
+        return "register";
     }
 }
