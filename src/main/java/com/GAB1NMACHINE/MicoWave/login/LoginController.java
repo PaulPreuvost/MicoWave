1542ed17b9a6f4eeb19538c4591d769843f7a52b
lucas <lucas.moreau@supinfo.com>
2024-05-17 11:05:43 +0200

    add fridge page

--- src/main/java/com/GAB1NMACHINE/MicoWave/login/LoginController.java
+++ src/main/java/com/GAB1NMACHINE/MicoWave/login/LoginController.java
@@ -1,13 +1,28 @@
 package com.GAB1NMACHINE.MicoWave.login;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping(path = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String loginPost(@RequestParam MultiValueMap<String, String> paramMap) {
+        String email = paramMap.getFirst("email");
+        String password = paramMap.getFirst("password");
+        if (email == null || password == null || email.isEmpty() || password.isEmpty()) {
+            return "login";
+        }
+        // Redirect to the home page
+        return "redirect:/";
     }
 }
